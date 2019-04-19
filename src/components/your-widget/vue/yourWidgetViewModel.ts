8e848c0d517712f6c5f7bb54b2e4f7a7540c753a
azaslonov <zaslonov.alexander@gmail.com>
2019-04-18 23:39:20 -0700

    tmp

---
+++ src/components/your-widget/vue/yourWidgetViewModel.ts
@@ -0,0 +1,8 @@
+import template from "./yourWidget.html";
+import { Component } from "@paperbits/common/vue/decorators";
+
+@Component({
+    selector: "your-widget",
+    template: template
+})
+export class YourWidgetViewModel { }
