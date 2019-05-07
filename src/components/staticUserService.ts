3f471e76b3d15012357a66c6fff4bc51e3887234
Igor O <ygrik@hotmail.com>
2019-05-07 10:40:16 -0700

    refactored user service

--- src/components/staticUserService.ts
+++ src/components/staticUserService.ts
@@ -8,6 +8,7 @@
 
 
 import { IUserService } from "@paperbits/common/user/IUserService";
+import { IUser } from "@paperbits/common/user/IUser";
 
 /**
  * Static user service for demo purposes.
@@ -15,7 +16,25 @@ import { IUserService } from "@paperbits/common/user/IUserService";
 export class StaticUserService implements IUserService {
     constructor() { }
 
-    public async getUserPhotoUrl(): Promise<string> {
-        return "https://cdn.paperbits.io/images/portrait.svg";
+    public async signIn(username: string, password: string): Promise<any> {
+        throw new Error("Method is not supported.");
+    }
+
+    public signOut(): void {
+        throw new Error("Method is not supported.");
+    }
+
+    public isCurrentUserLoggedIn(): boolean {
+        return true;
+    }
+
+    public async getCurrentUser(): Promise<IUser> {
+        return { 
+            id: "static",
+            displayName: "User",
+            email: "email@email.com",
+            claims: [],
+            photoUrl: "https://cdn.paperbits.io/images/portrait.svg" 
+        };
     }
 }
